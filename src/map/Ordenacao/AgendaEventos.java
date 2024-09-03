d9a84549cc8863c23203ac3e41562d99dfe3cabf
Guilherme Silva <sgsouza.173@gmail.com>
2024-09-03 15:28:50 -0300

    Finalizando o map
    Falta exercícios

--- src/map/Ordenacao/AgendaEventos.java
+++ src/map/Ordenacao/AgendaEventos.java
@@ -20,8 +20,38 @@ public class AgendaEventos {
     }
 
     public void obterProximoEvento() {
-        Set<LocalDate> dateSet = eventosMap.keySet();
-        Collection<Evento> values = eventosMap.values();
+        //Set<LocalDate> dateSet = eventosMap.keySet();
+        //Collection<Evento> values = eventosMap.values();
+
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()) {
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento +
+                        " acontecerá na data " + proximaData);
+                break;
+            }
+        }
     }
 
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        // Adiciona eventos à agenda
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 15), "Conferência de Tecnologia", "Palestrante renomado");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, 8, 28), "Hackathon de Inovação", "Competição de soluções criativas");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 9, 20), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
+
+        // Exibe a agenda completa de eventos
+        agendaEventos.exibirAgenda();
+
+        // Obtém e exibe o próximo evento na agenda
+        agendaEventos.obterProximoEvento();
+    }
 }
